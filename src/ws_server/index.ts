f196d4502385c9ce383abe5f008dd871a69fbd07
Adil_Askar <samask127node@gmail.com>
2022-06-25 14:14:12 +0600

    refactor: apply typescript

---
+++ src/ws_server/index.ts
@@ -0,0 +1,24 @@
+import WebSocket, { WebSocketServer, createWebSocketStream } from "ws";
+
+export const createWebSocketServer = async (portNumber: number) => {
+  const wss = new WebSocketServer({
+    port: portNumber,
+  });
+
+  wss.on("connection", async (wsClient: WebSocket) => {
+    const duplex = createWebSocketStream(wsClient, {
+      encoding: "utf8",
+      decodeStrings: false,
+    });
+
+    duplex.on("data", async (data: string) => {
+      console.log("data: ", data);
+
+      duplex.write("mouse_position 444,333\0", (error) => {
+        if (error) {
+          console.log(error);
+        }
+      });
+    });
+  });
+};
